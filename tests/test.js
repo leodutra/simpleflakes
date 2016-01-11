dc8621069b2d7566ea118bf7f95a4b9e7a2d9bbe
Leo Dutra <leodutra.br@gmail.com>
2016-01-11 19:32:41 -0200

    Enhanced test (TAP) logging with Faucet

--- tests/test.js
+++ tests/test.js
@@ -15,11 +15,11 @@ test('testing bn.js', function(t) {
 
   var bignum = new BigNum(SIMPLEFLAKE);
 
-  t.equal(bignum.toString(), SIMPLEFLAKE, 'stores-retrieves the right value');
-  t.equal(bignum.toString(), bignum.toString(10), 'toString() aliases toString(10)');
-  t.equal(bignum.toString(2), SIMPLEFLAKE_2, 'base 2 (binary)');
-  t.equal(bignum.toString(16), SIMPLEFLAKE_16, 'base 16');
-  t.equal(bignum.toString(36), SIMPLEFLAKE_36, 'base 36');
+  t.equal(bignum.toString(), SIMPLEFLAKE, 'bit number lib retrieves the right stored value?');
+  t.equal(bignum.toString(), bignum.toString(10), '<big number>.toString() aliases .toString(10)?');
+  t.equal(bignum.toString(2), SIMPLEFLAKE_2, 'big number as base 2?');
+  t.equal(bignum.toString(16), SIMPLEFLAKE_16, 'big number as base 16?');
+  t.equal(bignum.toString(36), SIMPLEFLAKE_36, 'big number as base 36?');
   t.end();
 
 });
@@ -27,19 +27,19 @@ test('testing bn.js', function(t) {
 
 test('testing lib.simpleflake()', function(t) {
 
-  t.assert(lib.simpleflake() instanceof  BigNum, 'returning correct instance of bn.js');
-  t.equal(lib.simpleflake(SIMPLEFLAKE_TIMESTAMP, SIMPLEFLAKE_RANDOMBITS, SIMPLEFLAKE_EPOCH).toString(), '4242436206093260245', 'right timestamp, random bits and epoch parameterization');
+  t.assert(lib.simpleflake() instanceof  BigNum, 'returning correct instance of big number lib?');
+  t.equal(lib.simpleflake(SIMPLEFLAKE_TIMESTAMP, SIMPLEFLAKE_RANDOMBITS, SIMPLEFLAKE_EPOCH).toString(), '4242436206093260245', 'right timestamp, random bits and epoch parameterization?');
   t.end();
 });
 
 test('testing lib.binary()', function(t) {
 
-  t.equal(lib.binary('83928382810918298'), '0000000100101010001011000110101101100100000001001000110110011010', "simpleflake.binary('83928382810918298')");
-  t.equal(lib.binary('83928382810918298', false), '100101010001011000110101101100100000001001000110110011010', "simpleflake.binary('83928382810918298', false)");
-  t.equal(lib.binary(7) ,  '0000000000000000000000000000000000000000000000000000000000000111', "simpleflake.binary(7)");
-  t.equal(lib.binary(7, false), '111', "simpleflake.binary(7, false)");
-  t.equal(lib.binary(64), '0000000000000000000000000000000000000000000000000000000001000000', "simpleflake.binary(64)");
-  t.equal(lib.binary(64, false), '1000000', "simpleflake.binary(64, false)");
+  t.equal(lib.binary('83928382810918298'), '0000000100101010001011000110101101100100000001001000110110011010', "valid simpleflake.binary('83928382810918298') result?");
+  t.equal(lib.binary('83928382810918298', false), '100101010001011000110101101100100000001001000110110011010', "valid simpleflake.binary('83928382810918298', false) result?");
+  t.equal(lib.binary(7) ,  '0000000000000000000000000000000000000000000000000000000000000111', "valid simpleflake.binary(7) result?");
+  t.equal(lib.binary(7, false), '111', "valid simpleflake.binary(7, false) result?");
+  t.equal(lib.binary(64), '0000000000000000000000000000000000000000000000000000000001000000', "valid simpleflake.binary(64) result?");
+  t.equal(lib.binary(64, false), '1000000', "valid simpleflake.binary(64, false) result?");
   t.end();
 });
 
@@ -53,8 +53,8 @@ test('testing lib.parseSimpleflake()', function(t) {
 
   var flake = lib.simpleflake(SIMPLEFLAKE_TIMESTAMP, SIMPLEFLAKE_RANDOMBITS);
 
-  t.equal(lib.parseSimpleflake(flake).timestamp, SIMPLEFLAKE_TIMESTAMP.toString(), 'correct timestamp parsing');
-  t.equal(lib.parseSimpleflake(flake).randomBits, SIMPLEFLAKE_RANDOMBITS.toString(), 'correct random bits parsing');
+  t.equal(lib.parseSimpleflake(flake).timestamp, SIMPLEFLAKE_TIMESTAMP.toString(), 'correct timestamp parsing?');
+  t.equal(lib.parseSimpleflake(flake).randomBits, SIMPLEFLAKE_RANDOMBITS.toString(), 'correct random bits parsing?');
 
   t.end();
 });
