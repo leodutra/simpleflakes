3034855e63562f32c9d3b8e9738c2fb9edecccd5
Leo Dutra <leodutra.br@gmail.com>
2019-08-06 19:47:55 -0300

    Update dependencies + eslint airbnb

--- benchmark/run.js
+++ benchmark/run.js
@@ -1,58 +1,58 @@
-'use strict'
+// eslint-disable-next-line import/no-extraneous-dependencies
+const Benchmark = require('benchmark');
 
-const Benchmark = require('benchmark')
-const suite = new Benchmark.Suite()
-const legacy = require('../lib/simpleflakes-legacy')
-const lib = require('../lib/simpleflakes')
-const BigNum = legacy.BigNum
+const suite = new Benchmark.Suite();
+const legacy = require('../lib/simpleflakes-legacy');
+const lib = require('../lib/simpleflakes');
 
-const SIMPLEFLAKE = '4242436206093260245'
-const SIMPLEFLAKE_EPOCH = 946702800000
-const SIMPLEFLAKE_TIMESTAMP = 1452440606092
-const SIMPLEFLAKE_RANDOMBITS = 7460309
+const { BigNum } = legacy;
 
-suite.add('simpleflake()', function() {
-  lib.simpleflake()
+const SIMPLEFLAKE = '4242436206093260245';
+const SIMPLEFLAKE_EPOCH = 946702800000;
+const SIMPLEFLAKE_TIMESTAMP = 1452440606092;
+const SIMPLEFLAKE_RANDOMBITS = 7460309;
+
+suite.add('simpleflake()', () => {
+  lib.simpleflake();
 })
-.add('simpleflake(parameterization)', function() {
-  lib.simpleflake(SIMPLEFLAKE_TIMESTAMP, SIMPLEFLAKE_RANDOMBITS, SIMPLEFLAKE_EPOCH)
-})
-.add('binary()', function() {
-  lib.binary(64)
-})
-.add('BigInt()', function() {
-  BigInt('4242436206093260245')
-})
-.add('parseSimpleflake()', function() {
-  lib.parseSimpleflake(SIMPLEFLAKE)
-})
+  .add('simpleflake(parameterization)', () => {
+    lib.simpleflake(SIMPLEFLAKE_TIMESTAMP, SIMPLEFLAKE_RANDOMBITS, SIMPLEFLAKE_EPOCH);
+  })
+  .add('binary()', () => {
+    lib.binary(64);
+  })
+  .add('BigInt()', () => {
+    // eslint-disable-next-line no-undef
+    BigInt('4242436206093260245');
+  })
+  .add('parseSimpleflake()', () => {
+    lib.parseSimpleflake(SIMPLEFLAKE);
+  });
 
 
 // legacy tests
-suite.add('legacy simpleflake()', function() {
-  legacy.simpleflake()
+suite.add('legacy simpleflake()', () => {
+  legacy.simpleflake();
 })
-.add('legacy simpleflake(parameterization)', function() {
-  legacy.simpleflake(SIMPLEFLAKE_TIMESTAMP, SIMPLEFLAKE_RANDOMBITS, SIMPLEFLAKE_EPOCH)
-})
-.add('legacy binary()', function() {
-  legacy.binary(64)
-})
-.add('legacy new BigNum()', function() {
-  new BigNum('4242436206093260245', 10)
-})
-.add('legacy parseSimpleflake()', function() {
-  legacy.parseSimpleflake(SIMPLEFLAKE)
-})
-
-
+  .add('legacy simpleflake(parameterization)', () => {
+    legacy.simpleflake(SIMPLEFLAKE_TIMESTAMP, SIMPLEFLAKE_RANDOMBITS, SIMPLEFLAKE_EPOCH);
+  })
+  .add('legacy binary()', () => {
+    legacy.binary(64);
+  })
+  .add('legacy new BigNum()', () => {
+    BigNum('4242436206093260245', 10);
+  })
+  .add('legacy parseSimpleflake()', () => {
+    legacy.parseSimpleflake(SIMPLEFLAKE);
+  })
 
 // add listeners
-.on('cycle', function(event) {
-  console.log(String(event.target))
-})
+  .on('cycle', (event) => {
+    console.log(String(event.target));
+  })
 // .on('complete', function() {
 //   console.log('Fastest is ' + this.filter('fastest').map('name'))
 // })
 // run async
-.run({ 'async': true })
+  .run({ async: true });
--- tests/simpleflakes.tests.js
+++ tests/simpleflakes.tests.js
@@ -1,72 +1,63 @@
-'use strict'
-
 if (typeof BigInt === 'function') {
-
-  const test = require('tape')
-  const lib = require('../lib/simpleflakes')
-
-  const SIMPLEFLAKE_EPOCH = 946702800000
-  const SIMPLEFLAKE_TIMESTAMP = 1452440606092
-  const SIMPLEFLAKE_RANDOMBITS = 7460309
-
-
-  test('testing simpleflake()', function(t) {
-    console.log(lib.simpleflake(), '---------------------------------')
-    t.assert(typeof lib.simpleflake() === 'bigint', 'returning BigInt?')
-    t.equal(lib.simpleflake(SIMPLEFLAKE_TIMESTAMP, SIMPLEFLAKE_RANDOMBITS, SIMPLEFLAKE_EPOCH).toString(), '4242436206093260245', 'right timestamp, random bits and epoch parameterization?')
-    t.end()
-  })
-
-  test('testing binary()', function(t) {
-
-    t.equal(lib.binary('83928382810918298'), '0000000100101010001011000110101101100100000001001000110110011010', "valid simpleflake.binary('83928382810918298') result?")
-    t.equal(lib.binary('83928382810918298', false), '100101010001011000110101101100100000001001000110110011010', "valid simpleflake.binary('83928382810918298', false) result?")
-    t.equal(lib.binary(7) ,  '0000000000000000000000000000000000000000000000000000000000000111', "valid simpleflake.binary(7) result?")
-    t.equal(lib.binary(7, false), '111', "valid simpleflake.binary(7, false) result?")
-    t.equal(lib.binary(64), '0000000000000000000000000000000000000000000000000000000001000000', "valid simpleflake.binary(64) result?")
-    t.equal(lib.binary(64, false), '1000000', "valid simpleflake.binary(64, false) result?")
-    t.end()
-  })
+  // eslint-disable-next-line global-require
+  const test = require('tape');
+  // eslint-disable-next-line global-require
+  const lib = require('../lib/simpleflakes');
+  const SIMPLEFLAKE_EPOCH = 946702800000;
+  const SIMPLEFLAKE_TIMESTAMP = 1452440606092;
+  const SIMPLEFLAKE_RANDOMBITS = 7460309;
 
 
-  test('testing extractBits()', function(t) {
+  test('testing simpleflake()', (t) => {
+    t.assert(typeof lib.simpleflake() === 'bigint', 'returning BigInt?');
+    t.equal(lib.simpleflake(SIMPLEFLAKE_TIMESTAMP, SIMPLEFLAKE_RANDOMBITS, SIMPLEFLAKE_EPOCH).toString(), '4242436206093260245', 'right timestamp, random bits and epoch parameterization?');
+    t.end();
+  });
 
-    t.assert(typeof lib.extractBits(7, 0, 1) === 'bigint', 'returns big int')
-    t.equal(lib.extractBits(7, 0, 1).toString(), '1', 'extractBits(7, 0, 1)')
-    t.equal(lib.extractBits(7, 0, 2).toString(), '3', 'extractBits(7, 0, 2)')
-    t.equal(lib.extractBits(7, 0, 3).toString(), '7', 'extractBits(7, 0, 3)')
-    t.equal(lib.extractBits(7, 1, 2).toString(), '3', 'extractBits(7, 1, 2)')
-    t.equal(lib.extractBits(7, 2, 1).toString(), '1', 'extractBits(7, 2, 1)')
-    t.equal(lib.extractBits(7, 2, 2).toString(), '1', 'extractBits(7, 2, 2)')
-    t.end()
-  })
+  test('testing binary()', (t) => {
+    t.equal(lib.binary('83928382810918298'), '0000000100101010001011000110101101100100000001001000110110011010', "valid simpleflake.binary('83928382810918298') result?");
+    t.equal(lib.binary('83928382810918298', false), '100101010001011000110101101100100000001001000110110011010', "valid simpleflake.binary('83928382810918298', false) result?");
+    t.equal(lib.binary(7), '0000000000000000000000000000000000000000000000000000000000000111', 'valid simpleflake.binary(7) result?');
+    t.equal(lib.binary(7, false), '111', 'valid simpleflake.binary(7, false) result?');
+    t.equal(lib.binary(64), '0000000000000000000000000000000000000000000000000000000001000000', 'valid simpleflake.binary(64) result?');
+    t.equal(lib.binary(64, false), '1000000', 'valid simpleflake.binary(64, false) result?');
+    t.end();
+  });
 
-  test('testing SimpleFlakeStruct()', function(t) {
-    t.assert(lib.SimpleFlakeStruct(SIMPLEFLAKE_TIMESTAMP.toString(), SIMPLEFLAKE_RANDOMBITS.toString()) instanceof lib.SimpleFlakeStruct, 'returning new SimpleFlakeStruct() when calling SimpleFlakeStruct()?')
-    t.throws(function() {
-      let undef
-      lib.SimpleFlakeStruct(undef, '1')
-    }, "throw typeError when timestamp arg is missing")
 
-    t.throws(function() {
-      lib.SimpleFlakeStruct('1')
-    }, "throw typeError when randomBits argument is missing")
+  test('testing extractBits()', (t) => {
+    t.assert(typeof lib.extractBits(7, 0, 1) === 'bigint', 'returns big int');
+    t.equal(lib.extractBits(7, 0, 1).toString(), '1', 'extractBits(7, 0, 1)');
+    t.equal(lib.extractBits(7, 0, 2).toString(), '3', 'extractBits(7, 0, 2)');
+    t.equal(lib.extractBits(7, 0, 3).toString(), '7', 'extractBits(7, 0, 3)');
+    t.equal(lib.extractBits(7, 1, 2).toString(), '3', 'extractBits(7, 1, 2)');
+    t.equal(lib.extractBits(7, 2, 1).toString(), '1', 'extractBits(7, 2, 1)');
+    t.equal(lib.extractBits(7, 2, 2).toString(), '1', 'extractBits(7, 2, 2)');
+    t.end();
+  });
 
-    t.throws(function() {
-      lib.SimpleFlakeStruct()
-    }, "throw typeError when arguments are missing")
+  test('testing SimpleFlakeStruct()', (t) => {
+    t.assert(lib.SimpleFlakeStruct(SIMPLEFLAKE_TIMESTAMP.toString(), SIMPLEFLAKE_RANDOMBITS.toString()) instanceof lib.SimpleFlakeStruct, 'returning new SimpleFlakeStruct() when calling SimpleFlakeStruct()?');
+    t.throws(() => {
+      let undef;
+      lib.SimpleFlakeStruct(undef, '1');
+    }, 'throw typeError when timestamp arg is missing');
 
-    t.end()
-  })
+    t.throws(() => {
+      lib.SimpleFlakeStruct('1');
+    }, 'throw typeError when randomBits argument is missing');
 
-  test('testing parseSimpleflake()', function(t) {
+    t.throws(() => {
+      lib.SimpleFlakeStruct();
+    }, 'throw typeError when arguments are missing');
 
-    const flake = lib.simpleflake(SIMPLEFLAKE_TIMESTAMP, SIMPLEFLAKE_RANDOMBITS)
+    t.end();
+  });
 
-    t.equal(lib.parseSimpleflake(flake).timestamp, SIMPLEFLAKE_TIMESTAMP.toString(), 'correct timestamp parsing?')
-    t.equal(lib.parseSimpleflake(flake).randomBits, SIMPLEFLAKE_RANDOMBITS.toString(), 'correct random bits parsing?')
-
-    t.end()
-  })
-
+  test('testing parseSimpleflake()', (t) => {
+    const flake = lib.simpleflake(SIMPLEFLAKE_TIMESTAMP, SIMPLEFLAKE_RANDOMBITS);
+    t.equal(lib.parseSimpleflake(flake).timestamp, SIMPLEFLAKE_TIMESTAMP.toString(), 'correct timestamp parsing?');
+    t.equal(lib.parseSimpleflake(flake).randomBits, SIMPLEFLAKE_RANDOMBITS.toString(), 'correct random bits parsing?');
+    t.end();
+  });
 }
